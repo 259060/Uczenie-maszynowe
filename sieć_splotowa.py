25b7cf530be3285b2d3ba761d48cee8d6b3b0b63
259060 <259060@student.pwr.edu.pl>
2025-12-31 14:14:12 +0100

    Create sieć_splotowa.py

---
+++ sieć_splotowa.py
@@ -0,0 +1,159 @@
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import numpy as np
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+
+def tworzenie_modelu_2_warstwy():
+  model = tf.keras.Sequential([
+      tf.keras.layers.Input(shape=(32, 32, 3)),
+      tf.keras.layers.Rescaling(1./255),
+
+      tf.keras.layers.Conv2D(32, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
+
+      tf.keras.layers.Conv2D(64, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
+
+      tf.keras.layers.Flatten(), 
+      tf.keras.layers.Dense(256, activation = 'relu'),
+      tf.keras.layers.Dense(10, activation = 'softmax')
+  ])
+  return model
+
+def tworzenie_modelu_3_warstwy():
+  model = tf.keras.Sequential([
+      tf.keras.layers.Input(shape=(32, 32, 3)),
+      tf.keras.layers.Rescaling(1./255),
+
+      tf.keras.layers.Conv2D(32, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
+
+      tf.keras.layers.Conv2D(64, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
+
+      tf.keras.layers.Conv2D(128, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
+
+      tf.keras.layers.Flatten(),
+      tf.keras.layers.Dense(256, activation = 'relu'),
+
+      tf.keras.layers.Dense(10, activation = 'softmax')
+  ])
+  return model
+
+def tworzenie_modelu_3_warstwy_dropout_15():
+  model = tf.keras.Sequential([
+      tf.keras.layers.Input(shape=(32, 32, 3)),
+      tf.keras.layers.Rescaling(1./255),
+
+      tf.keras.layers.Conv2D(32, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
+
+      tf.keras.layers.Conv2D(64, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
+
+      tf.keras.layers.Conv2D(128, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
+
+      tf.keras.layers.Flatten(),
+      tf.keras.layers.Dense(256, activation = 'relu'),
+      tf.keras.layers.Dropout(0.15),
+
+      tf.keras.layers.Dense(10, activation = 'softmax')
+  ])
+  return model
+
+def tworzenie_modelu_3_warstwy_dropout_25():
+  model = tf.keras.Sequential([
+      tf.keras.layers.Input(shape=(32, 32, 3)),
+      tf.keras.layers.Rescaling(1./255),
+
+      tf.keras.layers.Conv2D(32, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
+
+      tf.keras.layers.Conv2D(64, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
+
+      tf.keras.layers.Conv2D(128, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
+
+      tf.keras.layers.Flatten(),
+      tf.keras.layers.Dense(256, activation = 'relu'),
+      tf.keras.layers.Dropout(0.25),
+
+      tf.keras.layers.Dense(10, activation = 'softmax')
+  ])
+  return model
+  
+def tworzenie_modelu_3_warstwy_dropout_50():
+  model = tf.keras.Sequential([
+      tf.keras.layers.Input(shape=(32, 32, 3)),
+      tf.keras.layers.Rescaling(1./255),
+
+      tf.keras.layers.Conv2D(32, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
+
+      tf.keras.layers.Conv2D(64, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
+
+      tf.keras.layers.Conv2D(128, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
+
+      tf.keras.layers.Flatten(),
+      tf.keras.layers.Dense(256, activation = 'relu'),
+      tf.keras.layers.Dropout(0.50),
+
+      tf.keras.layers.Dense(10, activation = 'softmax')
+  ])
+  return model
+
+def tworzenie_modelu_3_warstwy_dropout_25_batch_normalization():
+  model = tf.keras.Sequential([
+      tf.keras.layers.Input(shape=(32, 32, 3)),
+      tf.keras.layers.Rescaling(1./255),
+
+      tf.keras.layers.Conv2D(32, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.BatchNormalization(),
+      tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
+
+      tf.keras.layers.Conv2D(64, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.BatchNormalization(),
+      tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
+
+      tf.keras.layers.Conv2D(128, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.BatchNormalization(),
+      tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
+
+      tf.keras.layers.Flatten(),
+      tf.keras.layers.Dense(256, activation = 'relu'),
+      tf.keras.layers.Dropout(0.50),
+
+      tf.keras.layers.Dense(10, activation = 'softmax')
+  ])
+  return model
+
+def modelowanie(model, epoki=20):
+
+    model.compile(
+      optimizer=keras.optimizers.Adam(3e-4),
+      loss=keras.losses.SparseCategoricalCrossentropy(),
+      metrics=['accuracy']
+    )
+
+    history = model.fit(
+        x_train, y_train,
+        epochs=epoki,
+        batch_size=64,
+        validation_data=(x_test, y_test)
+    )
+
+    return history
+
+modelowanie(tworzenie_modelu_2_warstwy())
+modelowanie(tworzenie_modelu_3_warstwy())
+modelowanie(tworzenie_modelu_3_warstwy_dropout_15())
+modelowanie(tworzenie_modelu_3_warstwy_dropout_25())
+modelowanie(tworzenie_modelu_3_warstwy_dropout_50())
+modelowanie(tworzenie_modelu_3_warstwy_dropout_25_batch_normalization())
