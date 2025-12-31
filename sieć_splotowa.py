56b9f67f632435ebb24a3be19af7b9eaec0017af
259060 <259060@student.pwr.edu.pl>
2025-12-31 14:32:35 +0100

    Update sieć_splotowa.py

--- sieć_splotowa.py
+++ sieć_splotowa.py
@@ -10,15 +10,19 @@ def tworzenie_modelu_2_warstwy():
       tf.keras.layers.Input(shape=(32, 32, 3)),
       tf.keras.layers.Rescaling(1./255),
 
-      tf.keras.layers.Conv2D(32, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.Conv2D(32, kernel_size = 3),
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
 
-      tf.keras.layers.Conv2D(64, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.Conv2D(64, kernel_size = 3),
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
 
       tf.keras.layers.Flatten(), 
-      tf.keras.layers.Dense(256, activation = 'relu'),
-      tf.keras.layers.Dense(10, activation = 'softmax')
+      tf.keras.layers.Dense(256), 
+      tf.keras.layers.Activation("relu"),
+      tf.keras.layers.Dense(10), 
+      tf.keras.layers.Activation("softmax")
   ])
   return model
 
@@ -27,19 +31,23 @@ def tworzenie_modelu_3_warstwy():
       tf.keras.layers.Input(shape=(32, 32, 3)),
       tf.keras.layers.Rescaling(1./255),
 
-      tf.keras.layers.Conv2D(32, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.Conv2D(32, kernel_size = 3),
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
 
-      tf.keras.layers.Conv2D(64, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.Conv2D(64, kernel_size = 3),
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
 
-      tf.keras.layers.Conv2D(128, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.Conv2D(128, kernel_size = 3),
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
 
       tf.keras.layers.Flatten(),
-      tf.keras.layers.Dense(256, activation = 'relu'),
-
-      tf.keras.layers.Dense(10, activation = 'softmax')
+      tf.keras.layers.Dense(256), 
+      tf.keras.layers.Activation("relu"),
+      tf.keras.layers.Dense(10), 
+      tf.keras.layers.Activation("softmax")
   ])
   return model
 
@@ -48,20 +56,24 @@ def tworzenie_modelu_3_warstwy_dropout_15():
       tf.keras.layers.Input(shape=(32, 32, 3)),
       tf.keras.layers.Rescaling(1./255),
 
-      tf.keras.layers.Conv2D(32, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.Conv2D(32, kernel_size = 3),
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
 
-      tf.keras.layers.Conv2D(64, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.Conv2D(64, kernel_size = 3),
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
 
-      tf.keras.layers.Conv2D(128, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.Conv2D(128, kernel_size = 3),
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
 
       tf.keras.layers.Flatten(),
-      tf.keras.layers.Dense(256, activation = 'relu'),
+      tf.keras.layers.Dense(256), 
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.Dropout(0.15),
-
-      tf.keras.layers.Dense(10, activation = 'softmax')
+      tf.keras.layers.Dense(10), 
+      tf.keras.layers.Activation("softmax")
   ])
   return model
 
@@ -70,20 +82,24 @@ def tworzenie_modelu_3_warstwy_dropout_25():
       tf.keras.layers.Input(shape=(32, 32, 3)),
       tf.keras.layers.Rescaling(1./255),
 
-      tf.keras.layers.Conv2D(32, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.Conv2D(32, kernel_size = 3),
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
 
-      tf.keras.layers.Conv2D(64, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.Conv2D(64, kernel_size = 3),
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
 
-      tf.keras.layers.Conv2D(128, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.Conv2D(128, kernel_size = 3),
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
 
       tf.keras.layers.Flatten(),
-      tf.keras.layers.Dense(256, activation = 'relu'),
+      tf.keras.layers.Dense(256), 
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.Dropout(0.25),
-
-      tf.keras.layers.Dense(10, activation = 'softmax')
+      tf.keras.layers.Dense(10), 
+      tf.keras.layers.Activation("softmax")
   ])
   return model
   
@@ -92,20 +108,24 @@ def tworzenie_modelu_3_warstwy_dropout_50():
       tf.keras.layers.Input(shape=(32, 32, 3)),
       tf.keras.layers.Rescaling(1./255),
 
-      tf.keras.layers.Conv2D(32, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.Conv2D(32, kernel_size = 3),
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
 
-      tf.keras.layers.Conv2D(64, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.Conv2D(64, kernel_size = 3),
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
 
-      tf.keras.layers.Conv2D(128, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.Conv2D(128, kernel_size = 3),
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
 
       tf.keras.layers.Flatten(),
-      tf.keras.layers.Dense(256, activation = 'relu'),
+      tf.keras.layers.Dense(256), 
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.Dropout(0.50),
-
-      tf.keras.layers.Dense(10, activation = 'softmax')
+      tf.keras.layers.Dense(10), 
+      tf.keras.layers.Activation("softmax")
   ])
   return model
 
@@ -114,23 +134,27 @@ def tworzenie_modelu_3_warstwy_dropout_25_batch_normalization():
       tf.keras.layers.Input(shape=(32, 32, 3)),
       tf.keras.layers.Rescaling(1./255),
 
-      tf.keras.layers.Conv2D(32, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.Conv2D(32, kernel_size = 3),
       tf.keras.layers.BatchNormalization(),
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
 
-      tf.keras.layers.Conv2D(64, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.Conv2D(64, kernel_size = 3),
       tf.keras.layers.BatchNormalization(),
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
 
-      tf.keras.layers.Conv2D(128, kernel_size = 3, activation = 'relu'),
+      tf.keras.layers.Conv2D(128, kernel_size = 3),
       tf.keras.layers.BatchNormalization(),
+      tf.keras.layers.Activation("relu"),
       tf.keras.layers.MaxPool2D(pool_size = 2, strides = 2),
 
       tf.keras.layers.Flatten(),
-      tf.keras.layers.Dense(256, activation = 'relu'),
-      tf.keras.layers.Dropout(0.50),
-
-      tf.keras.layers.Dense(10, activation = 'softmax')
+      tf.keras.layers.Dense(256), 
+      tf.keras.layers.Activation("relu"),
+      tf.keras.layers.Dropout(0.25),
+      tf.keras.layers.Dense(10), 
+      tf.keras.layers.Activation("softmax")
   ])
   return model
 
